3f6c20352cf06e51e26f8c1850d80ec601e9a232
Edward <ed828a@gmail.com>
2018-06-08 15:08:24 +0930

    Add NavigationView, NavigationDrawer, and Toolbar as ActionBar

--- app/src/main/java/com/dew/edward/fragmentexecise/MainActivity.kt
+++ app/src/main/java/com/dew/edward/fragmentexecise/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.dew.edward.fragmentexecise
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import com.dew.edward.fragmentexecise.fragments.FirstFragment
 import com.dew.edward.fragmentexecise.fragments.SecondFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val drawerToogle by lazy {
+        ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.drawer_open, R.string.drawer_close)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +25,25 @@ class MainActivity : AppCompatActivity() {
 
         val fragment = FirstFragment.newInstance()
         replaceFragment(fragment)
+
+        navigationView.setNavigationItemSelectedListener {
+            selectDrawerItem(it)
+            true
+        }
+
+        setSupportActionBar(toolbar)
+        drawerLayout.addDrawerListener(drawerToogle)
+    }
+
+    //configuration of synchronization between drawer toggle and drawer layout
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        drawerToogle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        drawerToogle.onConfigurationChanged(newConfig)
     }
 
     private fun replaceFragment(fragment: Fragment){
@@ -24,7 +52,24 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.commit()
     }
 
+    private fun selectDrawerItem(item: MenuItem){
+
+        val fragmentClass = when (item.itemId) {
+            R.id.firstFragment -> FirstFragment::class.java
+            R.id.secondFragment -> SecondFragment::class.java
+            else -> FirstFragment::class.java
+        }
+
+        replaceFragment(fragmentClass.newInstance())
+        drawerLayout.closeDrawer(GravityCompat.START)
+
+    }
+
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+//        Log.d("Options", "onOptionsItemSelected was hit")
+//        return if (drawerToogle.onOptionsItemSelected(item)) true else super.onOptionsItemSelected(item)
+
         return when(item?.itemId){
             R.id.firstFragment ->{
                 replaceFragment(FirstFragment.newInstance())
